789be6330a7ae8f99470e9546330421b6a162d83
Trung Hoang <sparkboldstudio@gmail.com>
2019-05-14 10:17:10 -0500

    add form items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FormContainer from './containers/FormContainer';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import FormContainer from "./containers/FormContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="col-md-6">
-        <h3> Sample Form Container </h3>
+        <h3> Email template form </h3>
         <FormContainer />
       </div>
     );
--- src/containers/FormContainer.jsx
+++ src/containers/FormContainer.jsx
@@ -1,33 +1,78 @@
-import React, {Component} from 'react';  
+import React, { Component } from "react";
 
 /* Import Components */
-import CheckBox from '../components/CheckBox';  
-import Input from '../components/Input';  
-import TextArea from '../components/TextArea';  
-import Select from '../components/Select';
-import Button from '../components/Button'
+import CheckBox from "../components/CheckBox";
+import Input from "../components/Input";
+import TextArea from "../components/TextArea";
+import Select from "../components/Select";
+import Button from "../components/Button";
 
-class FormContainer extends Component {  
+class FormContainer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      newUser: {
-        name: '',
-        age: '',
-        gender: '',
-        skills: [],
-        about: ''
-
+      newEmail: {
+        yourEmail: "",
+        departmentEmail: "",
+        deparment: "",
+        topics: [],
+        content: ""
       },
 
-      genderOptions: ['Male', 'Female', 'Others'],
-      skillOptions: ['Programming', 'Development', 'Design', 'Testing']
-
-    }
+      departmentOptions: [
+        "Client Experience Center of Excellence",
+        "Community Involvement",
+        "Corporate Human Resources",
+        "Corporate Real Estate & Facility Solutions",
+        "Employment Screening",
+        "Executive Office Group",
+        "Field Payroll Operations",
+        "Field Service Operations",
+        "Finance",
+        "Financial Solutions",
+        "Gross Profit Enhancement",
+        "Health & Welfare",
+        "Human Capital Solutions, Middle Market",
+        "Information Governance",
+        "Insurance Services",
+        "Legal",
+        "Marketing & Business Development",
+        "Payroll Service Operations",
+        "Payroll Services",
+        "Performance & Organizational Management",
+        "Pricing & Cost Analysis",
+        "Property & Casualty",
+        "Records Administration",
+        "Recruiting Services",
+        "Retirement Services",
+        "Sales - Business Performance Solutions",
+        "Sales - Executive VP",
+        "Sales - Inside Sales Support",
+        "Sales - Marketing",
+        "Sales - Office Management",
+        "Sales - Remote",
+        "Sales Operations",
+        "Service Operations",
+        "Service Operations - Contact Center",
+        "Service Operations - EEO",
+        "Service Operations - HR/COE",
+        "Service Operations Executive",
+        "Support Executive Group",
+        "Technology - Business Applications",
+        "Technology - Business Performance Solutions Tech",
+        "Technology - Enterprise Project Management",
+        "Technology - Infrastructure & Services",
+        "Technology - Product Innovation",
+        "Technology - VP",
+        "Time and Attendance",
+        "Traditional Employment Services"
+      ],
+      topicOptions: ["Programming", "Development", "Design", "Testing"]
+    };
     this.handleTextArea = this.handleTextArea.bind(this);
-    this.handleAge = this.handleAge.bind(this);
-    this.handleFullName = this.handleFullName.bind(this);
+    this.handleDepartmentEmail = this.handleDepartmentEmail.bind(this);
+    this.handleYourEmail = this.handleYourEmail.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleClearForm = this.handleClearForm.bind(this);
     this.handleCheckBox = this.handleCheckBox.bind(this);
@@ -35,157 +80,164 @@ class FormContainer extends Component {
   }
 
   /* This lifecycle hook gets executed when the component mounts */
-  
-  handleFullName(e) {
-   let value = e.target.value;
-   this.setState( prevState => ({ newUser : 
-        {...prevState.newUser, name: value
-        }
-      }), () => console.log(this.state.newUser))
+
+  handleYourEmail(e) {
+    let value = e.target.value;
+    this.setState(
+      prevState => ({ newEmail: { ...prevState.newEmail, yourEmail: value } }),
+      () => console.log(this.state.newEmail)
+    );
   }
 
-  handleAge(e) {
-       let value = e.target.value;
-   this.setState( prevState => ({ newUser : 
-        {...prevState.newUser, age: value
-        }
-      }), () => console.log(this.state.newUser))
+  handleDepartmentEmail(e) {
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        newEmail: { ...prevState.newEmail, departmentEmail: value }
+      }),
+      () => console.log(this.state.newEmail)
+    );
   }
 
   handleInput(e) {
-       let value = e.target.value;
-       let name = e.target.name;
-   this.setState( prevState => ({ newUser : 
-        {...prevState.newUser, [name]: value
-        }
-      }), () => console.log(this.state.newUser))
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState(
+      prevState => ({ newEmail: { ...prevState.newEmail, [name]: value } }),
+      () => console.log(this.state.newEmail)
+    );
   }
 
   handleTextArea(e) {
     console.log("Inside handleTextArea");
     let value = e.target.value;
-    this.setState(prevState => ({
-      newUser: {
-        ...prevState.newUser, about: value
-      }
-      }), ()=>console.log(this.state.newUser))
+    this.setState(
+      prevState => ({
+        newEmail: {
+          ...prevState.newEmail,
+          content: value
+        }
+      }),
+      () => console.log(this.state.newEmail)
+    );
   }
 
-
   handleCheckBox(e) {
-
     const newSelection = e.target.value;
     let newSelectionArray;
 
-    if(this.state.newUser.skills.indexOf(newSelection) > -1) {
-      newSelectionArray = this.state.newUser.skills.filter(s => s !== newSelection)
+    if (this.state.newEmail.topics.indexOf(newSelection) > -1) {
+      newSelectionArray = this.state.newEmail.topics.filter(
+        s => s !== newSelection
+      );
     } else {
-      newSelectionArray = [...this.state.newUser.skills, newSelection];
+      newSelectionArray = [...this.state.newEmail.topics, newSelection];
     }
 
-      this.setState( prevState => ({ newUser:
-        {...prevState.newUser, skills: newSelectionArray }
-      })
-      )
-}
+    this.setState(prevState => ({
+      newEmail: { ...prevState.newEmail, topics: newSelectionArray }
+    }));
+  }
 
   handleFormSubmit(e) {
     e.preventDefault();
-    let userData = this.state.newUser;
+    let userData = this.state.newEmail;
 
-    fetch('http://example.com',{
-        method: "POST",
-        body: JSON.stringify(userData),
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-      }).then(response => {
-        response.json().then(data =>{
-          console.log("Successful" + data);
-        })
-    })
-  }   
+    fetch("http://example.com", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      response.json().then(data => {
+        console.log("Successful" + data);
+      });
+    });
+  }
 
   handleClearForm(e) {
-  
-      e.preventDefault();
-      this.setState({ 
-        newUser: {
-          name: '',
-          age: '',
-          gender: '',
-          skills: [],
-          about: ''
-        },
-      })
+    e.preventDefault();
+    this.setState({
+      newEmail: {
+        yourEmail: "",
+        departmentEmail: "",
+        department: "",
+        topics: [],
+        content: ""
+      }
+    });
   }
 
   render() {
     return (
-    
-        <form className="container-fluid" onSubmit={this.handleFormSubmit}>
-       
-            <Input inputType={'text'}
-                   title= {'Full Name'} 
-                   name= {'name'}
-                   value={this.state.newUser.name} 
-                   placeholder = {'Enter your name'}
-                   handleChange = {this.handleInput}
-                   
-                   /> {/* Name of the user */}
-        
-          <Input inputType={'number'} 
-                name={'age'}
-                 title= {'Age'} 
-                 value={this.state.newUser.age} 
-                placeholder = {'Enter your age'}
-                 handleChange={this.handleAge} /> {/* Age */} 
-
-
-          <Select title={'Gender'}
-                  name={'gender'}
-                  options = {this.state.genderOptions} 
-                  value = {this.state.newUser.gender}
-                  placeholder = {'Select Gender'}
-                  handleChange = {this.handleInput}
-                  /> {/* Age Selection */}
-          <CheckBox  title={'Skills'}
-                  name={'skills'}
-                  options={this.state.skillOptions}
-                  selectedOptions = { this.state.newUser.skills}
-                  handleChange={this.handleCheckBox}
-                   /> {/* Skill */}
-          <TextArea
-            title={'About you.'}
-            rows={10}
-            value={this.state.newUser.about}
-            name={'currentPetInfo'}
-            handleChange={this.handleTextArea}
-            placeholder={'Describe your past experience and skills'} />{/* About you */}
-
-          <Button 
-              action = {this.handleFormSubmit}
-              type = {'primary'} 
-              title = {'Submit'} 
-            style={buttonStyle}
-          /> { /*Submit */ }
-          
-          <Button 
-            action = {this.handleClearForm}
-            type = {'secondary'}
-            title = {'Clear'}
-            style={buttonStyle}
-          /> {/* Clear the form */}
-          
-        </form>
-  
+      <form className="container-fluid" onSubmit={this.handleFormSubmit}>
+        <Input
+          inputType={"text"}
+          title={"Your Email"}
+          name={"yourEmail"}
+          value={this.state.newEmail.yourEmail}
+          placeholder={"firstname.lastname@insperity.com"}
+          handleChange={this.handleInput}
+        />{" "}
+        {/* yourEmail of the user */}
+        <Input
+          inputType={"number"}
+          name={"departmentEmail"}
+          title={"Department Email"}
+          value={this.state.newEmail.departmentEmail}
+          placeholder={"community.involvement@insperity.com)"}
+          handleChange={this.handleDepartmentEmail}
+        />{" "}
+        {/* departmentEmail */}
+        <Select
+          title={"Department"}
+          name={"department"}
+          options={this.state.departmentOptions}
+          value={this.state.newEmail.department}
+          placeholder={"Select department"}
+          handleChange={this.handleInput}
+        />{" "}
+        {/* departmentEmail Selection */}
+        {/* <CheckBox
+          title={"topics"}
+          name={"topics"}
+          options={this.state.topicOptions}
+          selectedOptions={this.state.newEmail.topics}
+          handleChange={this.handleCheckBox}
+        />{" "} */}
+        {/* Skill */}
+        <TextArea
+          title={"Email Content"}
+          rows={10}
+          value={this.state.newEmail.content}
+          name={"currentPetInfo"}
+          handleChange={this.handleTextArea}
+          placeholder={"Compose your message"}
+        />
+        {/* content of your message */}
+        <Button
+          action={this.handleFormSubmit}
+          type={"primary"}
+          title={"Submit"}
+          style={buttonStyle}
+        />{" "}
+        {/*Submit */}
+        <Button
+          action={this.handleClearForm}
+          type={"secondary"}
+          title={"Clear"}
+          style={buttonStyle}
+        />{" "}
+        {/* Clear the form */}
+      </form>
     );
   }
 }
 
 const buttonStyle = {
-  margin : '10px 10px 10px 10px'
-}
+  margin: "10px 10px 10px 10px"
+};
 
 export default FormContainer;
